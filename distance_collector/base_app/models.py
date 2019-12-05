a4402c53ee87f113a7b23d523ddb5ea7226afb74
cmos <simo95omis@gmail.com>
2019-12-05 17:33:47 +0100

    Distance_Collector commit #3

--- distance_collector/base_app/models.py
+++ distance_collector/base_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-#from django.utils import timezone
 from django.contrib.gis.db import models
 
 
@@ -8,10 +7,10 @@ class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
-    def __init__(self, first_name, last_name, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.first_name = first_name
-        self.last_name = last_name
+#    def __init__(self, first_name, last_name, *args, **kwargs):
+#        super().__init__(*args, **kwargs)
+#        self.first_name = first_name
+#        self.last_name = last_name
 
 
 class Car(models.Model):
@@ -20,11 +19,11 @@ class Car(models.Model):
     fuel_type = models.CharField(max_length=50)
     fuel_consumption = models.FloatField
 
-    def __init__(self, car_name, fuel_type, fuel_consumption, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.car_name = car_name
-        self.fuel_type = fuel_type
-        self.fuel_consumption = fuel_consumption
+#   def __init__(self, car_name, fuel_type, fuel_consumption, *args, **kwargs):
+#       super().__init__(*args, **kwargs)
+#       self.car_name = car_name
+#       self.fuel_type = fuel_type
+#       self.fuel_consumption = fuel_consumption
 
 
 class Place(models.Model):
@@ -32,9 +31,9 @@ class Place(models.Model):
     coordinates = models.PointField
     # to be defined
 
-    def __init__(self, address, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.address = address
+#   def __init__(self, address, *args, **kwargs):
+#       super().__init__(*args, **kwargs)
+#       self.address = address
 
     def set_coordinates(self):
         return
@@ -45,23 +44,23 @@ class Place(models.Model):
 
 
 class Passenger(Person):
-    starting_point = Place
+    starting_point = models.CharField(max_length=100, default='')
     destination_point = Place
     time_of_appearance = models.TimeField
     endurance_time = models.DurationField
 
-    def __init__(self, first_name, last_name, starting_point=None, destination_point=None, *args, **kwargs):
-        super().__init__(first_name, last_name, *args, **kwargs)
-        self.starting_point = starting_point
-        self.destination_point = destination_point
+#   def __init__(self, first_name, last_name, starting_point=None, destination_point=None, *args, **kwargs):
+#       super().__init__(first_name, last_name, *args, **kwargs)
+#       self.starting_point = starting_point
+#       self.destination_point = destination_point
 
 
 class Driver(Passenger):
     car = Car
     is_driving = models.BooleanField
 
-    def __init__(self, first_name, last_name, starting_point=None, destination_point=None, *args, **kwargs):
-        super().__init__(first_name, last_name, starting_point, destination_point, *args, **kwargs)
+#   def __init__(self, first_name, last_name, starting_point=None, destination_point=None, *args, **kwargs):
+#       super().__init__(first_name, last_name, starting_point, destination_point, *args, **kwargs)
         # create new Car
 
 
@@ -73,11 +72,11 @@ class Trip(models.Model):
     date = models.DateField
     arrival_time = models.TimeField
 
-    def __init__(self, destination, date, arrival_time,  *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.destination = destination
-        self.date = date
-        self.arrival_time = arrival_time
+#   def __init__(self, destination, date, arrival_time,  *args, **kwargs):
+#       super().__init__(*args, **kwargs)
+#       self.destination = destination
+#       self.date = date
+#       self.arrival_time = arrival_time
 
     def add_passenger(self, passenger):
         self.passengers.add(passenger)
--- distance_collector/base_app/views.py
+++ distance_collector/base_app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.forms import modelformset_factory
+from .models import *
 
 
 def collecting_data_view(request):
-    return render(request, 'base_app/collecting_data_template.html', {})
+
+    passenger_formset = modelformset_factory(Passenger, fields=('first_name', 'last_name', 'starting_point'))
+    driver_formset = modelformset_factory(Driver, fields=('first_name', 'last_name', 'starting_point'))
+    #if request.method == 'POST'['passengers submit']:
+    if 'passengers submit' in request.POST:
+        form_p = passenger_formset(request.POST)
+
+        instances_p = form_p.save()
+
+        #instances = form.save(commit=False)
+        #for instance in instances:
+        #        instance.save()
+
+    #if request.method == 'POST'['drivers submit']:
+    if 'drivers submit' in request.POST:
+        form_d = driver_formset(request.POST)
+        instances_d = form_d.save()
+
+    form_p = passenger_formset()
+    form_d = driver_formset()
+    return render(request, 'base_app/collecting_data_template.html', {'form_p': form_p, 'form_d': form_d})
